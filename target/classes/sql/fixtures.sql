3cae3c7a92ccf4de97793d9b6c36ee97da8b9e4c
Khozzy <khozzy@gmail.com>
2013-09-11 20:48:23 +0200

    Updated .gitignore

--- target/classes/sql/fixtures.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO SeedPage(active, fetch_date, response_code, url, version) VALUES (1, NOW(), 202, 'http://www.intenso.pl',1);
-INSERT INTO SeedPage(active, fetch_date, response_code, url, version) VALUES (1, NOW(), 202, 'http://www.adaptly.com',1);
-INSERT INTO SeedPage(active, fetch_date, response_code, url, version) VALUES (1, NOW(), 202, 'http://www.fastwhitecat.com',1);
